8a21aa83c449e88664fd226e0152a29bf305826d
Emilien Chapon <emilien.chapon@cern.ch>
2016-03-02 19:08:03 +0100

    A few improvements to the bkgStudy macro

--- Fitter/Macros/bkgStudy.C
+++ Fitter/Macros/bkgStudy.C
@@ -15,23 +15,27 @@
 using namespace std;
 using namespace RooFit;
 
-void bkgStudy() {
+int cFactor = 2; // 1 for regular LLR test, 2 for Akaike Information Criterion (AIC)
+
+void bkgStudy(bool isPbPb = false) {
    vector<string> filenames;
+   // Background functions with the smallest number of parameters should go first
+   filenames.push_back("Workspace/DATA/ws_DATA_Psi2SJpsi_PPPrompt_pt65300_rap1624_cent0200_262620_263757_Exponential.root");
    filenames.push_back("Workspace/DATA/ws_DATA_Psi2SJpsi_PPPrompt_pt65300_rap1624_cent0200_262620_263757_FirstOrderChebychev.root");
    filenames.push_back("Workspace/DATA/ws_DATA_Psi2SJpsi_PPPrompt_pt65300_rap1624_cent0200_262620_263757_SecondOrderChebychev.root");
    filenames.push_back("Workspace/DATA/ws_DATA_Psi2SJpsi_PPPrompt_pt65300_rap1624_cent0200_262620_263757_ThirdOrderChebychev.root");
    filenames.push_back("Workspace/DATA/ws_DATA_Psi2SJpsi_PPPrompt_pt65300_rap1624_cent0200_262620_263757_FourthOrderChebychev.root");
-   filenames.push_back("Workspace/DATA/ws_DATA_Psi2SJpsi_PPPrompt_pt65300_rap1624_cent0200_262620_263757_Exponential.root");
    
-   const char* fcnname = "pdfMASS_Bkg_PP";
-   const char* fcnname_full = "pdfMASS_Tot_PP";
+   const char* fcnname = isPbPb ? "pdfMASS_Bkg_PbPb" : "pdfMASS_Bkg_PP";
+   const char* fcnname_full = isPbPb ? "pdfMASS_Tot_PbPb" : "pdfMASS_Tot_PP";
+   const char* dataname = isPbPb ? "dOS_DATA_PbPb" : "dOS_DATA_PP";
 
    vector<string> fcntags;
+   fcntags.push_back("Exponential");
    fcntags.push_back("FirstOrderChebychev");
    fcntags.push_back("SecondOrderChebychev");
    fcntags.push_back("ThirdOrderChebychev");
    fcntags.push_back("FourthOrderChebychev");
-   fcntags.push_back("Exponential");
 
    vector<double> nlls;
    vector<int> npars;
@@ -41,21 +45,25 @@ void bkgStudy() {
    for (unsigned int i=0; i<filenames.size(); i++) {
       TFile *tfile = new TFile(filenames[i].c_str());
       RooWorkspace *ws = (RooWorkspace*) tfile->Get("myws");
-      RooAbsReal *nll =ws->pdf(fcnname_full)->createNLL(*ws->data("dOS_DATA_PP"));
+      RooAbsReal *nll =ws->pdf(fcnname_full)->createNLL(*ws->data(dataname));
       // ws->pdf(fcnname)->fitTo(*ws->data("dOS_DATA_PP"), SumW2Error(kTRUE), Extended(kTRUE), Save(), NumCPU(8), Range("MassWindow"),NormRange("MassWindow"));
       nlls.push_back(nll->getVal());
-      int npar = ws->pdf(fcnname)->getParameters(*ws->data("dOS_DATA_PP"))->getSize();
+      int npar = ws->pdf(fcnname)->getParameters(*ws->data(dataname))->getSize();
       npars.push_back(npar);
    }
 
    // print results
    for (unsigned int ibkg=0; ibkg<filenames.size(); ibkg++) {
-      cout << fcntags[ibkg] << " (" << npars[ibkg] << ") " << ((ibkg==0) ? nlls[ibkg] : 2.*(nlls[ibkg]-nlls[0]));
+      cout.setf(ios::fixed, ios::floatfield);
+      // cout << fcntags[ibkg] << " (" << npars[ibkg] << ") " << setprecision(2) << ((ibkg==0) ? nlls[ibkg] : 2.*(nlls[ibkg]-nlls[0]));
+      cout << fcntags[ibkg] << " (" << npars[ibkg] << ") " << setprecision(2) << nlls[ibkg];
+      cout << setprecision(4);
+      cout.unsetf(ios::floatfield);
       for (unsigned int ibkg2=0; ibkg2<ibkg; ibkg2++) {
-         double deltanll = 2.*fabs(nlls[ibkg]-nlls[ibkg2]);
+         double deltanll = -2.*(nlls[ibkg]-nlls[ibkg2]);
          int deltanpar = abs(npars[ibkg]-npars[ibkg2]);
-         double prob = 100.*TMath::Prob(deltanll,deltanpar);
-         cout << "\t" << 100.*TMath::Prob(deltanll,deltanpar);
+         double prob = deltanll > 0 ? 100.*TMath::Prob(deltanll,cFactor*deltanpar) : 100;
+         cout << "\t" <<  prob;
       } // for (ibkg2)
       cout << endl;
    } // for (ibkg)
