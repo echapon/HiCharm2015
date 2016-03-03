219f0a277b49189bb136657764e106dc8299a5e0
Emilien Chapon <emilien.chapon@cern.ch>
2016-03-03 19:31:54 +0100

    Fix a few typos in the script

--- Fitter/Input/createInput.py
+++ Fitter/Input/createInput.py
@@ -9,28 +9,31 @@ from shutil import copyfile
 
 bins=[
         "0.0-1.6;6.5-30.0;0.0-100.0",
-        "0.0-1.6;6.5-8;0.0-100.0",
-        "0.0-1.6;8-9.5;0.0-100.0",
-        "0.0-1.6;9.5-30;0.0-100.0"
         ]
 
 # provide the signal model for pp and PbPb
 fcn_signal_pp="DoubleCrystalBall"
 fcn_signal_pbpb="DoubleCrystalBall"
+fcn_signal2_pp="DoubleCrystalBall"
+fcn_signal2_pbpb="DoubleCrystalBall"
 
 # provide the background model for pp and PbPb
 fcn_bkg_pp="SecondOrderChebychev"
 fcn_bkg_pbpb="SecondOrderChebychev"
 
 # provide the name of the parameters of your models
-parnames_signal_pp="N_Jpsi_PbPb;sigma1_Jpsi_PbPb;sigma2_Jpsi_PbPb;m_Jpsi_PbPb;alpha_Jpsi_PbPb;n_Jpsi_PbPb;f_Jpsi_PbPb"
+parnames_signal_pp="N_Jpsi_PP;sigma1_Jpsi_PP;sigma2_Jpsi_PP;m_Jpsi_PP;alpha_Jpsi_PP;n_Jpsi_PP;f_Jpsi_PP"
 parnames_signal_pbpb="N_Jpsi_PbPb;sigma1_Jpsi_PbPb;sigma2_Jpsi_PbPb;m_Jpsi_PbPb;alpha_Jpsi_PbPb;n_Jpsi_PbPb;f_Jpsi_PbPb"
-parnames_bkg_pp="N_Bkg_PbPb;lambda1_Bkg_PbPb;lambda2_Bkg_PbPb;lambda3_Bkg_PbPb;lambda4_Bkg_PbPb"
+parnames_signal2_pp="N_Psi2S_PP;sigma1_Psi2S_PP;sigma2_Psi2S_PP;m_Psi2S_PP;alpha_Psi2S_PP;n_Psi2S_PP;f_Psi2S_PP"
+parnames_signal2_pbpb="N_Psi2S_PbPb;sigma1_Psi2S_PbPb;sigma2_Psi2S_PbPb;m_Psi2S_PbPb;alpha_Psi2S_PbPb;n_Psi2S_PbPb;f_Psi2S_PbPb"
+parnames_bkg_pp="N_Bkg_PP;lambda1_Bkg_PP;lambda2_Bkg_PP;lambda3_Bkg_PP;lambda4_Bkg_PP"
 parnames_bkg_pbpb="N_Bkg_PbPb;lambda1_Bkg_PbPb;lambda2_Bkg_PbPb;lambda3_Bkg_PbPb;lambda4_Bkg_PbPb"
 
 # provide the initial parameters
 parini_signal_pp="[ 0 , 250000 ];[ 0.04 , 0.01 , 0.09 ];[ 0.02 , 0.01 , 0.07 ];[ 3.096 , 2.996 , 3.196 ];[ 2.21 , 0.5 , 3000.0 ];[ 2.0 , 0.5 , 3000.0 ];[ 0.3 , 0.0 , 1.0 ]"
 parini_signal_pbpb="[ 0 , 250000 ];[ 0.04 , 0.01 , 0.09 ];[ 0.02 , 0.01 , 0.07 ];[ 3.096 , 2.996 , 3.196 ];[ 2.21 , 0.5 , 3000.0 ];[ 2.0 , 0.5 , 3000.0 ];[ 0.3 , 0.0 , 1.0 ]"
+parini_signal2_pp=";;;;;;"
+parini_signal2_pbpb=";;;;;;"
 parini_bkg_pp="[0,300000];[0.05,-1.0,1.0];[0.05,-1.0,1.0];;"
 parini_bkg_pbpb="[ 0 , 250000 ];[ 0.05 , -1.0 , 1.0 ];[ 0.05 , -1.0 , 1.0 ];;"
 
@@ -53,24 +56,36 @@ os.mkdir(name)
 
 # signal, pp
 file_sig_pp = open(name + '/InitialParam_MASS_JPSI_PP.csv', 'w')
-file_sig_pp.write('rap;pt;cent;Model_JPSI_PP;' + parnames_signal_pp + '\n')
+file_sig_pp.write('rap;pt;cent;Model_Jpsi_PP;' + parnames_signal_pp + '\n')
 for strbin in bins:
     file_sig_pp.write(strbin + ';' + fcn_signal_pp + ';' + parini_signal_pp + '\n')
 
 # signal, pbpb
 file_sig_pbpb = open(name + '/InitialParam_MASS_JPSI_PbPb.csv', 'w')
-file_sig_pbpb.write('rap;pt;cent;Model_JPSI_PbPb;' + parnames_signal_pbpb + '\n')
+file_sig_pbpb.write('rap;pt;cent;Model_Jpsi_PbPb;' + parnames_signal_pbpb + '\n')
 for strbin in bins:
     file_sig_pbpb.write(strbin + ';' + fcn_signal_pbpb + ';' + parini_signal_pbpb + '\n')
 
+# signal2, pp
+file_sig_pp = open(name + '/InitialParam_MASS_PSI2S_PP.csv', 'w')
+file_sig_pp.write('rap;pt;cent;Model_Psi2S_PP;' + parnames_signal2_pp + '\n')
+for strbin in bins:
+    file_sig_pp.write(strbin + ';' + fcn_signal2_pp + ';' + parini_signal2_pp + '\n')
+
+# signal2, pbpb
+file_sig_pbpb = open(name + '/InitialParam_MASS_PSI2S_PbPb.csv', 'w')
+file_sig_pbpb.write('rap;pt;cent;Model_Psi2S_PbPb;' + parnames_signal2_pbpb + '\n')
+for strbin in bins:
+    file_sig_pbpb.write(strbin + ';' + fcn_signal2_pbpb + ';' + parini_signal2_pbpb + '\n')
+
 # bkg, pp
-file_sig_pp = open(name + '/InitialParam_MASS_Bkg_PP.csv', 'w')
+file_sig_pp = open(name + '/InitialParam_MASS_BKG_PP.csv', 'w')
 file_sig_pp.write('rap;pt;cent;Model_Bkg_PP;' + parnames_bkg_pp + '\n')
 for strbin in bins:
     file_sig_pp.write(strbin + ';' + fcn_bkg_pp + ';' + parini_bkg_pp + '\n')
 
 # bkg, pbpb
-file_sig_pbpb = open(name + '/InitialParam_MASS_Bkg_PbPb.csv', 'w')
+file_sig_pbpb = open(name + '/InitialParam_MASS_BKG_PbPb.csv', 'w')
 file_sig_pbpb.write('rap;pt;cent;Model_Bkg_PbPb;' + parnames_bkg_pbpb + '\n')
 for strbin in bins:
     file_sig_pbpb.write(strbin + ';' + fcn_bkg_pbpb + ';' + parini_bkg_pbpb + '\n')
