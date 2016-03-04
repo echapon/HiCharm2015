4777128bf461aaa41c75892270ed82e8d32c3004
Emilien Chapon <emilien.chapon@cern.ch>
2016-03-04 17:08:13 +0100

    Make things work outside CERN

---
+++ Fitter/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# setup ROOT 6 (if you have access to AFS)
+
+source /afs/cern.ch/sw/lcg/external/gcc/4.9/x86_64-slc6-gcc49-opt/setup.sh 
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.04.02/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh 
