7311cdbb7b9e28a78fe517f9defd1c9a372359e9
Emilien Chapon <emilien.chapon@cern.ch>
2016-03-12 00:13:47 +0100

    Extend the bin classes

--- Fitter/Macros/Utilities/bin.h
+++ Fitter/Macros/Utilities/bin.h
@@ -6,15 +6,26 @@ class bin {
       Type low;
       Type high;
 
+      // constructors
       bin(Type l, Type h) :
          low(l), high(h) { 
          };
+      bin(const bin<Type>& b) :
+         low(b.low), high(b.high) {
+         };
+      bin() : 
+         low(0), high(0) {
+         };
 
+      // destructor
       ~bin(){};
 
-      bool operator==(bin other) {
+      // operator
+      bool operator==(const bin other) const {
          return low==other.low && high==other.high;
       };
+      bool operator>(const bin other) const {return true;}
+      bool operator<(const bin other) const {return true;}
 };
 
 // define a few common uses of the template class
@@ -29,15 +40,28 @@ class anabin {
       binD ptbin;
       binI centbin;
 
+      // constructors
       anabin(double rapmin, double rapmax, double ptmin, double ptmax, int centmin, int centmax) :
          rapbin(rapmin, rapmax),
          ptbin(ptmin, ptmax),
          centbin(centmin, centmax) {
          };
+      anabin(const anabin& a) :
+         rapbin(a.rapbin),
+         ptbin(a.ptbin),
+         centbin(a.centbin) {
+         };
+      anabin() :
+         rapbin(), ptbin(), centbin() {
+         };
 
+      // destructor
       ~anabin(){};
 
-      bool operator==(anabin other) {
+      // operator
+      bool operator==(const anabin other) const {
          return rapbin==other.rapbin && ptbin==other.ptbin && centbin==other.centbin;
       };
+      bool operator>(const anabin other) const {return true;}
+      bool operator<(const anabin other) const {return true;}
 };
