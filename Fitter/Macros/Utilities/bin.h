b51bbd3de8639258b901258c09a2f7bd71b2d781
Emilien Chapon <emilien.chapon@cern.ch>
2016-03-13 02:30:28 +0100

    It starts to look like something

--- Fitter/Macros/Utilities/bin.h
+++ Fitter/Macros/Utilities/bin.h
@@ -1,67 +1,35 @@
+#ifndef bin_h
+#define bin_h
+
+#include <utility>
+#include <tuple>
+
+using namespace std;
+
 // a simple template class to store a bin and overload the equality operator
 
-template <typename Type> 
-class bin {
-   public:
-      Type low;
-      Type high;
-
-      // constructors
-      bin(Type l, Type h) :
-         low(l), high(h) { 
-         };
-      bin(const bin<Type>& b) :
-         low(b.low), high(b.high) {
-         };
-      bin() : 
-         low(0), high(0) {
-         };
-
-      // destructor
-      ~bin(){};
-
-      // operator
-      bool operator==(const bin other) const {
-         return low==other.low && high==other.high;
-      };
-      bool operator>(const bin other) const {return true;}
-      bool operator<(const bin other) const {return true;}
-};
-
 // define a few common uses of the template class
+template <typename T> class bin : public pair<T,T> {
+   public:
+      bin(T a, T b) : pair<T,T>(a,b) {};
+      T low() const {return this->first;}
+      T high() const {return this->second;}
+};
 typedef bin<double> binD;
 typedef bin<float> binF;
 typedef bin<int> binI;
 
 // associate three such bins to make an analysis bin
-class anabin {
+class anabin : public tuple<binD,binD,binI> {
    public:
-      binD rapbin;
-      binD ptbin;
-      binI centbin;
-
-      // constructors
       anabin(double rapmin, double rapmax, double ptmin, double ptmax, int centmin, int centmax) :
-         rapbin(rapmin, rapmax),
-         ptbin(ptmin, ptmax),
-         centbin(centmin, centmax) {
-         };
-      anabin(const anabin& a) :
-         rapbin(a.rapbin),
-         ptbin(a.ptbin),
-         centbin(a.centbin) {
-         };
-      anabin() :
-         rapbin(), ptbin(), centbin() {
-         };
-
-      // destructor
-      ~anabin(){};
-
-      // operator
-      bool operator==(const anabin other) const {
-         return rapbin==other.rapbin && ptbin==other.ptbin && centbin==other.centbin;
-      };
-      bool operator>(const anabin other) const {return true;}
-      bool operator<(const anabin other) const {return true;}
+         tuple<binD,binD,binI> (binD(rapmin,rapmax), binD(ptmin, ptmax), binI(centmin, centmax)) {};
+      binD rapbin() const {return get<0>(*this);};
+      binD ptbin() const {return get<1>(*this);};
+      binI centbin() const {return get<2>(*this);};
+      void setrapbin(binD rapbin) {get<0>(*this) = rapbin;};
+      void setptbin(binD ptbin) {get<1>(*this) = ptbin;};
+      void setcentbin(binI centbin) {get<2>(*this) = centbin;};
 };
+
+#endif // #ifndef bin_h
--- Fitter/plotResults.C
+++ Fitter/plotResults.C
@@ -15,6 +15,8 @@
 #include "TLine.h"
 #include "RooRealVar.h"
 #include "RooWorkspace.h"
+#include "TSystemDirectory.h"
+#include "TSystemFile.h"
 
 using namespace std;
 
@@ -29,24 +31,25 @@ anabin binFromFile(const char* filename);
 bool binok(vector<anabin> thecats, string xaxis, anabin &tocheck);
 // plot
 void plotGraph(map<anabin, TGraphAsymmErrors*> theGraphs, string xaxis, string outputDir);
-void plot(vector<anabin> thecats, string xaxis, string outputDir);
+void plot(vector<anabin> thecats, string xaxis, string workDirName);
+vector<TString> fileList(const char* input);
 
-void plotPt(string outputDir) {
+void plotPt(string workDirName) {
    string xaxis = "pt";
    vector<anabin> theCats;
    theCats.push_back(anabin(0,1.6,6.5,30,0,200));
    theCats.push_back(anabin(1.6,2.4,3,30,0,200));
 
-   plot(theCats,xaxis,outputDir);
+   plot(theCats,xaxis,workDirName);
 };
 
-void plotCent(string outputDir) {
+void plotCent(string workDirName) {
    string xaxis = "cent";
    vector<anabin> theCats;
    theCats.push_back(anabin(0,1.6,6.5,30,0,200));
    theCats.push_back(anabin(1.6,2.4,3,30,0,200));
 
-   plot(theCats,xaxis,outputDir);
+   plot(theCats,xaxis,workDirName);
 };
 
 
@@ -55,15 +58,13 @@ void plot(vector<anabin> thecats, string xaxis, string outputDir) {
    // xaxis is the variable to be plotted. "pt", "rap" or "cent"
 
    // list of files
-   vector<const char*> theFiles;
-   theFiles.push_back("");
-   theFiles.push_back("");
+   vector<TString> theFiles = fileList(outputDir.c_str());
 
    map<anabin, RooRealVar*> theVars;
 
-   for (vector<const char*>::const_iterator it=theFiles.begin(); it!=theFiles.end(); it++) {
-      anabin thebin = binFromFile(*it);
-      theVars[thebin] = poiFromFile(*it);
+   for (vector<TString>::const_iterator it=theFiles.begin(); it!=theFiles.end(); it++) {
+      anabin thebin = binFromFile(it->Data());
+      theVars[thebin] = poiFromFile(it->Data());
    }
 
    map<anabin, vector<anabin> > theBins;
@@ -84,6 +85,7 @@ void plot(vector<anabin> thecats, string xaxis, string outputDir) {
    }
 
    // make TGraphAsymmErrors
+   int cnt=0;
    for (vector<anabin>::const_iterator it=thecats.begin(); it!=thecats.end(); it++) {
       int n = theBins[*it].size();
       if(n==0) {
@@ -92,10 +94,11 @@ void plot(vector<anabin> thecats, string xaxis, string outputDir) {
          continue;
       }
       theGraphs[*it] = new TGraphAsymmErrors(n);
+      theGraphs[*it]->SetName(Form("bin_%i",cnt));
       for (int i=0; i<n; i++) {
          double x, exl, exh, y, eyl, eyh;
-         double low = theBins[*it][i].ptbin.low;
-         double high = theBins[*it][i].ptbin.high;
+         double low = theBins[*it][i].ptbin().low();
+         double high = theBins[*it][i].ptbin().high();
          if (xaxis=="pt") {
             x = (low+high)/2.;
             exh = (high-low)/2.;
@@ -111,7 +114,9 @@ void plot(vector<anabin> thecats, string xaxis, string outputDir) {
          eyh = theVarsBinned[*it][i]->getErrorHi();
          theGraphs[*it]->SetPoint(i,x,y);
          theGraphs[*it]->SetPointError(i,exl,exh,eyl,eyh);
+         cout << x << " " << y << " " << endl;
       }
+      cnt++;
    }
 
    // plot
@@ -120,18 +125,38 @@ void plot(vector<anabin> thecats, string xaxis, string outputDir) {
 
 anabin binFromFile(const char* filename) {
    TFile *f = new TFile(filename);
+   if (!f) {
+      cout << "Error, file " << filename << " does not exist." << endl;
+      return anabin(0,0,0,0,0,0);
+   }
    RooWorkspace *ws = (RooWorkspace*) f->Get("workspace");
+   if (!ws) {
+      cout << "Error, file " << filename << " is bad." << endl;
+      return anabin(0,0,0,0,0,0);
+   }
    RooRealVar *pt = (RooRealVar*) ws->var("pt");
    RooRealVar *rap = (RooRealVar*) ws->var("rap");
    RooRealVar *cent = (RooRealVar*) ws->var("cent");
-   anabin ans(pt->getMin(),pt->getMax(),rap->getMin(),rap->getMax(),cent->getMin(),cent->getMax());
+   if (!pt || !rap || !cent) {
+      cout << "Error, file " << filename << " is bad." << endl;
+      return anabin(0,0,0,0,0,0);
+   }
+   anabin ans(rap->getMin(),rap->getMax(),pt->getMin(),pt->getMax(),cent->getMin(),cent->getMax());
    f->Close(); delete f;
    return ans;
 }
 
 RooRealVar* poiFromFile(const char* filename) {
    TFile *f = new TFile(filename);
+   if (!f) {
+      cout << "Error, file " << filename << " does not exist." << endl;
+      return NULL;
+   }
    RooWorkspace *ws = (RooWorkspace*) f->Get("workspace");
+   if (!ws) {
+      cout << "Error, file " << filename << " is bad." << endl;
+      return NULL;
+   }
    RooRealVar *ans = (RooRealVar*) ws->var(poiname);
    RooRealVar* ansc = new RooRealVar(*ans,Form("%s_from_%s",poiname,filename));
    f->Close(); delete f;
@@ -142,13 +167,15 @@ bool binok(vector<anabin> thecats, string xaxis, anabin &tocheck) {
    bool ok=false;
 
    for (vector<anabin>::const_iterator it=thecats.begin(); it!=thecats.end(); it++) {
-      if (xaxis=="pt" && it->rapbin==tocheck.rapbin && it->centbin==tocheck.centbin) {
+      if (xaxis=="pt" && it->rapbin()==tocheck.rapbin() && it->centbin()==tocheck.centbin()
+            && ! (it->ptbin()==tocheck.ptbin())) {
          ok=true;
-         tocheck.ptbin = it->ptbin;
+         tocheck.setptbin(it->ptbin());
          break;
-      } else if (xaxis=="cent" && it->rapbin==tocheck.rapbin && it->ptbin==tocheck.ptbin) {
+      } else if (xaxis=="cent" && it->rapbin()==tocheck.rapbin() && it->ptbin()==tocheck.ptbin()
+            && ! (it->centbin()==tocheck.centbin())) {
          ok=true;
-         tocheck.centbin = it->centbin;
+         tocheck.setcentbin(it->centbin());
          break;
       }
    }
@@ -196,11 +223,11 @@ void plotGraph(map<anabin, TGraphAsymmErrors*> theGraphs, string xaxis, string o
          tg->SetMarkerColor(kGreen);
          tg->SetLineColor(kGreen);
       }
-      tg->Draw("AP");      
+      tg->Draw("P");      
 
-      TString raplabel = Form("%.1f < |y| < %.1f ; ",it->first.rapbin.low,it->first.rapbin.high);
+      TString raplabel = Form("%.1f < |y| < %.1f ; ",it->first.rapbin().low(),it->first.rapbin().high());
       TString otherlabel = "BWAA";
-      if (xaxis == "pt") otherlabel = Form("%i%-%i%",(int) (it->first.centbin.low/2.), (int) (it->first.centbin.high/2.));
+      if (xaxis == "pt") otherlabel = Form("%i\%-%i\%",(int) (it->first.centbin().low()/2.), (int) (it->first.centbin().high()/2.));
       tleg->AddEntry(tg, raplabel + otherlabel, "p");
 
       cnt++;
@@ -214,12 +241,36 @@ void plotGraph(map<anabin, TGraphAsymmErrors*> theGraphs, string xaxis, string o
    c1->cd();
    c1->Update();
    c1->RedrawAxis();
-   gSystem->mkdir(Form("%splot/RESULT/root/", outputDir.c_str()), kTRUE); 
-   c1->SaveAs(Form("%splot/RESULT/root/result_%s.root",outputDir.c_str(), xaxis.c_str()));
-   gSystem->mkdir(Form("%splot/RESULT/png/", outputDir.c_str()), kTRUE);
-   c1->SaveAs(Form("%splot/RESULT/png/result_%s.png",outputDir.c_str(), xaxis.c_str()));
-   gSystem->mkdir(Form("%splot/RESULT/pdf/", outputDir.c_str()), kTRUE);
-   c1->SaveAs(Form("%splot/RESULT/pdf/result_%s.pdf",outputDir.c_str(), xaxis.c_str()));
+   gSystem->mkdir(Form("Output/%s/plot/RESULT/root/", outputDir.c_str()), kTRUE); 
+   c1->SaveAs(Form("Output/%s/plot/RESULT/root/result_%s.root",outputDir.c_str(), xaxis.c_str()));
+   gSystem->mkdir(Form("Output/%s/plot/RESULT/png/", outputDir.c_str()), kTRUE);
+   c1->SaveAs(Form("Output/%s/plot/RESULT/png/result_%s.png",outputDir.c_str(), xaxis.c_str()));
+   gSystem->mkdir(Form("Output/%s/plot/RESULT/pdf/", outputDir.c_str()), kTRUE);
+   c1->SaveAs(Form("Output/%s/plot/RESULT/pdf/result_%s.pdf",outputDir.c_str(), xaxis.c_str()));
 
    delete c1;
+}
+
+vector<TString> fileList(const char* input) {
+   vector<TString> ans;
+
+   TString basedir(Form("Output/%s/result/DATA/",input));
+   TSystemDirectory dir(input,basedir);
+
+   TList *files = dir.GetListOfFiles();
+
+   if (files) {
+      TIter next(files);
+      TSystemFile *file;
+      TString fname;
+
+      while ((file=(TSystemFile*)next())) {
+         fname = file->GetName();
+         if (fname.EndsWith(".root")) {
+            ans.push_back(basedir+fname);
+         }
+      }
+   }
+
+   return ans;
 }
